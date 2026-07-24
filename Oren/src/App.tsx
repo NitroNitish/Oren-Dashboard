eb9fd0964b85a1f36580ad6dad32d78c60608e01
NitroNitish <nitroshubham21@gmail.com>
2026-07-24 23:29:35 +0530

    Add Vercel Analytics and remove logo from Rates page

--- Oren/src/App.tsx
+++ Oren/src/App.tsx
@@ -1,5 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -31,6 +32,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
+          <VercelAnalytics />
           <BrowserRouter>
             <Routes>
               <Route path="/auth" element={<Auth />} />
